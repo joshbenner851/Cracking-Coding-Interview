de974fccdf73656e3008457deb08a395c4db7086
Joshua Benner <bennerster@gmail.com>
2016-06-17 00:40:57 -0400

    Finished Compressing Strings

--- stringCompression.py
+++ stringCompression.py
@@ -1,29 +1,37 @@
-def compressString(string):
-    
-    if(len(string) < 2):
-        return false
+def compressString(compressing):
+    #edge case for length
+    if(len(compressing) <= 2):
+        return False
+    #initializations
     compressedString = ""
-    lastChar = string[0]
-    reps = 0
-    for x in range(0,len(string)):
-        if(string[x] == lastChar):
-            reps += 1
+    lastChar = compressing[0]
+    reps = 1
+    isFirst = True
+    #aaabb = a3b2 < 5 letters
+    for x in range(0,len(compressing)):
+        if(compressing[x] != lastChar or x == len(compressing)):
+            compressedString +=  lastChar + str(reps)
+            reps = 1
+            isFirst = True
         else:
-            compressedString += lastChar + str(reps)
-            reps = 0
-        lastChar = string[x]
+            if(isFirst):
+                isFirst = False
+            else:
+                reps +=1
+        lastChar = compressing[x]
+    compressedString += lastChar + str(reps)
     #safety check for edge case
-    if(len(compressedString) >= len(string)):
+    if(len(compressedString) >= len(compressing)):
         return False
     else:
         return compressedString
 
-string = input("Please enter a string for compression")
+string = str(input("Please enter a string for compression"))
 while(string != "exit"):
     result = compressString(string)
     if(result == False):
         print(string + " couldn't be compressed any smaller")
     else:
         print(string + " was compressed down to " + result)
-    string = input("Please enter a string for compression")
+    string = str(input("Please enter a string for compression or exit to quit the program"))
 
